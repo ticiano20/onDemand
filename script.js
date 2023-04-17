cf191efca495bdfdee9c18318aa50ea83f8ec0c1
ticiano20 <barabinoticiano@gmail.com>
2023-04-17 20:27:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Navbar
+const menuToggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
